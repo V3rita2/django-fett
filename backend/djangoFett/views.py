077c908f8a99d5708fa5dacc21e0570edd30241d
veritas <rose.daemon.belmont@gmail.com>
2021-11-06 11:47:06 -0700

    adds base splash page

--- backend/djangoFett/views.py
+++ backend/djangoFett/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import UpdateView, CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import DetailView
 
 # Create your views here.
+
+# splashpage view
+
+class Home(TemplateView):
+    template_name = "home.html"
