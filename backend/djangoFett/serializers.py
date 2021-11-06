e6a5745241e51376c1c48cbecb7b2c55dda9d151
veritas <rose.daemon.belmont@gmail.com>
2021-11-06 11:59:41 -0700

    for letiing react read the models

---
+++ backend/djangoFett/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+# from .models import PUT MODELS HERE
+
+#EXAMPLE OF HOW TO SERIALIZE
+# class ModelSerializer(serializers.ModelSerializer) :
+        # class Meta:
+            # model = Model
+            # fields = ('field1', 'field2',)
+#REMEMBER TO UPDATE ANY VIEWS USING THIS
