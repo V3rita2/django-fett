0155058fadedb4ec6a6765d185a4422e4f41ea0c
veritas <rose.daemon.belmont@gmail.com>
2021-11-06 14:49:51 -0700

    first track model

--- MelodicManager/mercenary/models.py
+++ MelodicManager/mercenary/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.fields import CharField, DateTimeField
 
-# Create your models here.
+#test model for tracks
+
+class Track(models.Model):
+    title = CharField(max_length=200)
+    image = CharField(max_length=1000, blank=True)
+    created_at = DateTimeField(auto_now_add=True)
