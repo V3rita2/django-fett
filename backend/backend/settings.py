7d0fc07f2a93c9243f1895f277b305de44b57c76
veritas <rose.daemon.belmont@gmail.com>
2021-11-06 11:53:39 -0700

    undid everything cause I forgot how dr stack worked

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djangoFett',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +127,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cross Origin Resource sharing 
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
--- backend/djangoFett/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    #splash page path
-    path('', views.Home.as_view(), name="home"),
-    # auth signup path
-    path('signup/', views.Signup.as_view(), name="signup"),
-]
