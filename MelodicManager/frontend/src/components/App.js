c4c0886378640906ec9588fc73bb6afd20e815a5
veritas <rose.daemon.belmont@gmail.com>
2021-11-12 17:03:45 -0800

    adds some basic react components

--- MelodicManager/frontend/src/components/App.js
+++ MelodicManager/frontend/src/components/App.js
@@ -1,12 +1,17 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
 
 import Header from "./layout/Header";
-
+import Home from "./mercenary/Home";
 class App extends Component {
     render() {
         return (
-            <Header />
+            <Fragment>
+                <Header />
+                <div className="container">
+                    <Home />
+                </div>
+            </Fragment>
         )
     }
 }
---
+++ MelodicManager/frontend/src/components/mercenary/Home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import Form from './Form'
+import Tracks from './Tracks'
+
+export default function Home() {
+    return (
+        <Fragment>
+            <Form />
+            <Tracks />
+        </Fragment>
+    )
+}
